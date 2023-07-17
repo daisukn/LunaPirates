d7b289997c55bd1534095eaeaaf3904d0553506b
Daisuke <daisukn@outlook.com>
2023-07-17 20:06:05 +0900

    Initial Commit

---
+++ LunaPirates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LunaPirates
+//
+//  Created by Daisuke Nishimori on 2023/07/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
